93baa58fa6e8c902dc15b79df9b03f9b3a40de88
Pranav Miglani <72353081+PranavMiglani45@users.noreply.github.com>
2021-08-17 22:38:49 +0530

    Create ReplacePi.cpp

---
+++ Recursion/ReplacePi.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+void replace(char input[],int len,int s){
+    if(s==len-1){
+        return;
+    }
+   replace(input,len,s+1);
+    if(input[s]=='p' && input[s+1]=='i'){
+    //shift
+        for(int i=strlen(input);i>=s+2;i--){
+            input[i+2]=input[i];
+        }
+    //value
+    input[s]='3';
+        input[s+1]='.';
+        input[s+2]='1';
+        input[s+3]='4';
+    }
+}
+void replacePi(char input[]) {
+	// Write your code here
+   int len=strlen(input);
+    replace(input,len,0);
+}
