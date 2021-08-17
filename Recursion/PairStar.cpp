3c25ac3a54f505483fe65f843b4958e7e12d41c9
Pranav Miglani <72353081+PranavMiglani45@users.noreply.github.com>
2021-08-17 22:40:22 +0530

    Create PairStar.cpp

---
+++ Recursion/PairStar.cpp
@@ -0,0 +1,15 @@
+#include<cstring>
+void pairStar(char input[]) {
+    // Write your code here
+   int len = strlen(input);
+    if(len==0){
+        return;
+    }
+    if(input[0]==input[1]){
+        for(int i=len;i>0;i--){
+        input[i+1]=input[i];
+        }
+        input[1]='*';
+    }
+    pairStar(input+1);
+}
